7f44a3cea3cd807300f3794c1033bf911618de82
mahesh <rudrakshalamahesh12@gmail.com>
2024-01-12 08:24:15 +0530

    test

---
+++ my-web-app/src/main/java/com/example/MyApp.java
@@ -0,0 +1,10 @@
+// src/main/java/com/example/MyApp.java
+
+package com.example;
+
+public class MyApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java World!");
+    }
+}
+
